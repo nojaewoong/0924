bf05b1df266dac569698880c73eea07de6c36743
nojaewoong <55523998+nojaewoong@users.noreply.github.com>
2020-09-24 17:04:44 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            String ipt;
+            Console.WriteLine("hello world");
+            Console.WriteLine("정수 입력 : ");
+            Console.ReadLine();
+            ipt = Console.ReadLine();
+
+            num1 = int.Parse(ipt);
+            Console.WriteLine(num1);
+
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("첫번쟤 숫자 : ");
+            abc:
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("두번 쨰 숫자 : ");
+
+            Console.WriteLine(num1 + num2 + "=" +(num1 + num2));
+            Console.WriteLine(num1 - num2 + "=" +(num1 + num2));
+            Console.WriteLine(num1 / num2 + "=" + (num1 + num2));
+            Console.WriteLine(num1 * num2 + "=" + (num1 + num2));
+            goto abc;
+        }
+    }
+}
